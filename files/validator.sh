96a86a6554200bbe091f240b24f537b99c54b64e
dedoom <enzopizarroaguiar@Gmail.com>
2024-04-18 12:03:50 -0400

    reorganizing

--- files/validator.sh
+++ files/validator.sh
@@ -10,26 +10,30 @@ exec /mnt/solana/target/release/solana-validator \
 --rpc-port 8899 \
 --dynamic-port-range 8002-8099 \
 --no-port-check \
---halt-on-trusted-validators-accounts-hash-mismatch \
 --gossip-port 8001 \
---no-voting \
---private-rpc \
 --rpc-bind-address 0.0.0.0 \
+--halt-on-trusted-validators-accounts-hash-mismatch \
+--wal-recovery-mode skip_any_corrupted_record \
+--no-voting \
+--no-untrusted-rpc \
+--no-os-network-limits-test \
+--no-duplicate-instance-check \
+--private-rpc \
 --enable-cpi-and-log-storage \
+--enable-rpc-transaction-history \
 --account-index program-id \
 --account-index spl-token-owner \
---enable-rpc-transaction-history \
---wal-recovery-mode skip_any_corrupted_record \
+--accounts-index-memory-limit-mb 350 \
 --log /mnt/logs/solana-validator.log \
 --accounts /mnt/solana-accounts \
 --ledger /mnt/solana-ledger \
 --snapshots /mnt/solana-snapshots \
---expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
 --rpc-send-default-max-retries 30 \
 --rpc-send-service-max-retries 30 \
 --rpc-send-retry-ms 2000 \
+--limit-ledger-size \
 --full-rpc-api \
---accounts-index-memory-limit-mb 350 \
+--expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
 --account-index-exclude-key kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6 \
 --account-index-exclude-key metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s \
 --known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
