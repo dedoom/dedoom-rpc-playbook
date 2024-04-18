b72f5de59f99913eed0c68cf210b2e78c6d25b74
dedoom <enzopizarroaguiar@Gmail.com>
2024-04-18 11:31:56 -0400

    updating some configs

--- files/validator.sh
+++ files/validator.sh
@@ -25,13 +25,13 @@ exec /mnt/solana/target/release/solana-validator \
 --ledger /mnt/solana-ledger \
 --snapshots /mnt/solana-snapshots \
 --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
---rpc-send-default-max-retries 5 \
---rpc-send-service-max-retries 5 \
+--rpc-send-default-max-retries 30 \
+--rpc-send-service-max-retries 30 \
 --rpc-send-retry-ms 2000 \
 --full-rpc-api \
 --accounts-index-memory-limit-mb 350 \
 --account-index-exclude-key kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6 \
---tpu-use-quic \
+--account-index-exclude-key metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s \
 --known-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
 --known-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
 --known-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
